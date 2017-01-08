d5c58a240d16313b39b0bb4adca8f045b1895f54
Mario Viapiano <marioeviapiano@gmail.com>
2017-01-07 22:57:27 -0300

    Finished application

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"flag"
-	"fmt"
-	"log"
-	"net"
 	"os"
+
+	"github.com/emeve89/mychat/lib"
 )
 
 func main() {
@@ -16,36 +14,10 @@ func main() {
 	flag.Parse()
 
 	if isHost {
-		connIP := os.Args[3]
-		runHost(connIP)
-	} else {
 		connIP := os.Args[2]
-		runGuest(connIP)
+		lib.RunHost(connIP)
+	} else {
+		connIP := os.Args[1]
+		lib.RunGuest(connIP)
 	}
-}
-
-const port = "8080"
-
-func runHost(ip string) {
-	ipAndPort := ip + ":" + port
-	listener, listenErr := net.Listen("tcp", ipAndPort)
-	if listenErr != nil {
-		log.Fatal("Error", listenErr)
-	}
-	conn, acceptErr := listener.Accept()
-	if acceptErr != nil {
-		log.Fatal("Error", acceptErr)
-	}
-
-	reader := bufio.NewReader(conn)
-	message, readErr := reader.ReadString('\n')
-	if readErr != nil {
-		log.Fatal("Error", readErr)
-	}
-
-	fmt.Println("Message received: ", message)
-}
-
-func runGuest(ip string) {
-
 }
