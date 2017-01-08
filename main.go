eaee60427855ffe8bc5ace0b501c4dd7a3943603
Mario Viapiano <marioeviapiano@gmail.com>
2017-01-07 22:17:49 -0300

    Add runHost function

--- main.go
+++ main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello")
+	var isHost bool
+
+	flag.BoolVar(&isHost, "listen", false, "Listens on the specified IP address.")
+	flag.Parse()
+
+	if isHost {
+		connIP := os.Args[3]
+		runHost(connIP)
+	} else {
+		connIP := os.Args[2]
+		runGuest(connIP)
+	}
+}
+
+const port = "8080"
+
+func runHost(ip string) {
+	ipAndPort := ip + ":" + port
+	listener, listenErr := net.Listen("tcp", ipAndPort)
+	if listenErr != nil {
+		log.Fatal("Error", listenErr)
+	}
+	conn, acceptErr := listener.Accept()
+	if acceptErr != nil {
+		log.Fatal("Error", acceptErr)
+	}
+
+	reader := bufio.NewReader(conn)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error", readErr)
+	}
+
+	fmt.Println("Message received: ", message)
+}
+
+func runGuest(ip string) {
+
 }
